523c24f653e0ac395f70d4ecff3c627341426e04
Nicholas W <nclwgn@gmail.com>
2022-02-05 20:00:56 -0300

    Calling Fauna register creation in sign in callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,23 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+
+        return true;
+      }
+      catch {
+        return false;
+      }
+    }
+  }
 });
